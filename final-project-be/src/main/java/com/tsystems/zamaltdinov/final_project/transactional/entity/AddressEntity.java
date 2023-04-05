89bb0a577096e88e9d70c28c7169a19617b37fe7
user_dinho <ramilinter@gmail.com>
2023-04-05 11:44:08 +0200

    client -> user

--- final-project-be/src/main/java/com/tsystems/zamaltdinov/final_project/transactional/entity/AddressEntity.java
+++ final-project-be/src/main/java/com/tsystems/zamaltdinov/final_project/transactional/entity/AddressEntity.java
@@ -1,10 +1,18 @@
 package com.tsystems.zamaltdinov.final_project.transactional.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.util.Collection;
 import java.util.UUID;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 @Entity
 @Table(name = "address", schema = "store", catalog = "postgres")
 public class AddressEntity {
@@ -13,8 +21,8 @@ public class AddressEntity {
     @Column(name = "id")
     private UUID id;
     @Basic
-    @Column(name = "client_id")
-    private UUID clientId;
+    @Column(name = "user_id")
+    private UUID userId;
     @Basic
     @Column(name = "country")
     private String country;
@@ -34,126 +42,18 @@ public class AddressEntity {
     @Column(name = "apartment")
     private String apartment;
     @ManyToOne
-    @JoinColumn(name = "client_id", referencedColumnName = "id", insertable=false, updatable=false)
-    private ClientEntity clientByClientId;
+    @JoinColumn(name = "user_id", referencedColumnName = "id", insertable=false, updatable=false)
+    private User userByUserId;
     @OneToMany(mappedBy = "addressByAddressId")
     private Collection<OrderEntity> ordersById;
 
-    public AddressEntity() {}
 
-    public AddressEntity(UUID id, UUID clientId, String country, String city, Integer postalCode, String street, Integer home, String apartment) {
-        this.id = id;
-        this.clientId = clientId;
-        this.country = country;
-        this.city = city;
-        this.postalCode = postalCode;
-        this.street = street;
-        this.home = home;
-        this.apartment = apartment;
+    public User getUserByUserId() {
+        return userByUserId;
     }
 
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public UUID getClientId() {
-        return clientId;
-    }
-
-    public void setClientId(UUID clientId) {
-        this.clientId = clientId;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public Integer getPostalCode() {
-        return postalCode;
-    }
-
-    public void setPostalCode(Integer postalCode) {
-        this.postalCode = postalCode;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public Integer getHome() {
-        return home;
-    }
-
-    public void setHome(Integer home) {
-        this.home = home;
-    }
-
-    public String getApartment() {
-        return apartment;
-    }
-
-    public void setApartment(String apartment) {
-        this.apartment = apartment;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        AddressEntity that = (AddressEntity) o;
-
-        if (id != null ? !id.equals(that.id) : that.id != null) return false;
-        if (clientId != null ? !clientId.equals(that.clientId) : that.clientId != null) return false;
-        if (country != null ? !country.equals(that.country) : that.country != null) return false;
-        if (city != null ? !city.equals(that.city) : that.city != null) return false;
-        if (postalCode != null ? !postalCode.equals(that.postalCode) : that.postalCode != null) return false;
-        if (street != null ? !street.equals(that.street) : that.street != null) return false;
-        if (home != null ? !home.equals(that.home) : that.home != null) return false;
-        if (apartment != null ? !apartment.equals(that.apartment) : that.apartment != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (clientId != null ? clientId.hashCode() : 0);
-        result = 31 * result + (country != null ? country.hashCode() : 0);
-        result = 31 * result + (city != null ? city.hashCode() : 0);
-        result = 31 * result + (postalCode != null ? postalCode.hashCode() : 0);
-        result = 31 * result + (street != null ? street.hashCode() : 0);
-        result = 31 * result + (home != null ? home.hashCode() : 0);
-        result = 31 * result + (apartment != null ? apartment.hashCode() : 0);
-        return result;
-    }
-
-    public ClientEntity getClientByClientId() {
-        return clientByClientId;
-    }
-
-    public void setClientByClientId(ClientEntity clientByClientId) {
-        this.clientByClientId = clientByClientId;
+    public void setUserbyUserId (User userByUserId) {
+        this.userByUserId = userByUserId;
     }
 
     public Collection<OrderEntity> getOrdersById() {
