9f5b821c77daa392301f14d9f49fdb16c6b33b33
user_dinho <ramilinter@gmail.com>
2023-04-06 10:44:06 +0200

    fixed logout issue

--- final-project-be/src/main/java/com/tsystems/zamaltdinov/final_project/rest/controller/CorsConfig.java
+++ final-project-be/src/main/java/com/tsystems/zamaltdinov/final_project/rest/controller/CorsConfig.java
@@ -4,12 +4,15 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsConfig extends OncePerRequestFilter {
 
     @Override
