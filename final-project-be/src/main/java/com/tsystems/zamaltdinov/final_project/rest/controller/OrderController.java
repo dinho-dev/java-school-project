2d13dc4c17ef04d77ed00961dd98232950e2dd7d
user_dinho <ramilinter@gmail.com>
2023-04-08 10:10:14 +0200

    changed localhost

--- final-project-be/src/main/java/com/tsystems/zamaltdinov/final_project/rest/controller/OrderController.java
+++ final-project-be/src/main/java/com/tsystems/zamaltdinov/final_project/rest/controller/OrderController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:5432")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/orders")
 public class OrderController {
