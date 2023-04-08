d9595d9b8b674134d51c6f6a1889cd6b36014432
user_dinho <ramilinter@gmail.com>
2023-04-08 10:09:42 +0200

    deleted todo

--- final-project-be/src/main/java/com/tsystems/zamaltdinov/final_project/rest/controller/ProductController.java
+++ final-project-be/src/main/java/com/tsystems/zamaltdinov/final_project/rest/controller/ProductController.java
@@ -7,14 +7,15 @@ import com.tsystems.zamaltdinov.final_project.transactional.repository.ProductRe
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/v1/products")
 public class ProductController {
     @Autowired
@@ -43,7 +44,6 @@ public class ProductController {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-    // TODO: create add to cart order
 
     @PutMapping("/{id}")
     public ResponseEntity<ProductDTO> updateProduct(@PathVariable("id") UUID id, @RequestBody ProductDTO productDTO) {
